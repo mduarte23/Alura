1581932676bb53a37ce7e4ed5cd3915dab1ff2fe
Marcelo Duarte <126211070+mduarte23@users.noreply.github.com>
2025-06-27 13:20:45 -0300

    orgonograma

---
+++ React/orgonograma/src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,16 @@
+import "./ListaSuspensa.css";
+
+const ListaSuspensa = (props) => {
+    return (
+        <div>
+            <label>
+                {props.label}
+            </label>
+            <select>
+                
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa;
